1568948bd1ffc4ae903b493fd73d6e0cfd1d6a88
coolCicada <1475395755@qq.com>
2023-01-24 15:00:11 +0800

    737

---
+++ c++入门以及简单的顺序结构/p/737.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int a[10];
+  
+  for (int i = 0; i < 10; i ++) {
+    cin >> a[i];
+  }
+
+  for (int i = 0; i < 10; i ++) {
+    if (a[i] <= 0) a[i] = 1;
+  }
+
+  for (int i = 0; i < 10; i ++) {
+    printf("X[%d] = %d\n", i, a[i]);
+  }
+
+  return 0;
+}
