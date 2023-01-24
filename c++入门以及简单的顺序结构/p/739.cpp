f4c3f0c737cd36902540658a8c12cb837a75f9cb
coolCicada <1475395755@qq.com>
2023-01-24 15:08:45 +0800

    739

---
+++ c++入门以及简单的顺序结构/p/739.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  double arr[100];
+
+  for (int i = 0; i < 100; i ++) {
+    cin >> arr[i];
+  }
+
+  for (int i = 0; i < 100; i ++) {
+    if (arr[i] <= 10) {
+      printf("A[%d] = %.1lf\n", i, arr[i]);
+    }
+  }
+
+  return 0;
+}
