9747968990a3ef064b1c73ba93b86f449297627a
coolCicada <1475395755@qq.com>
2023-01-25 18:18:44 +0800

    feat:添加双指针解法

--- c++入门以及简单的顺序结构/p/766.cpp
+++ c++入门以及简单的顺序结构/p/766.cpp
@@ -3,10 +3,29 @@ using namespace std;
 
 int main() {
   // cin 做法
-  string str;
+  /* string str;
   while(cin >> str) {
     cout << str << ' ';
+  } */
+
+  // 双指针做法
+  string str;
+  getline(cin, str);
+
+  string res;
+
+  for (int i = 0; i < str.size(); i ++) {
+    res += str[i];
+    
+    if (str[i] != ' ') continue;
+
+    int j = i;
+    while (j < str.size() && str[j] == ' ') j ++;
+    i = j - 1;
   }
+
+  cout << res << endl;
+
   // 常规做法
   /* string str;
   getline(cin, str);
