cf5aa4dba46931b7a01db6fe57e901f5e8637dfb
coolCicada <1475395755@qq.com>
2023-01-25 17:58:37 +0800

    771

---
+++ c++入门以及简单的顺序结构/p/771.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+
+  while (n --) {
+    string str;
+    cin >> str;
+
+    int cnt = 0;
+    char c;
+
+    for (int i = 0; i < str.size(); i ++) {
+      int j = i;
+      while (j < str.size() && str[j] == str[i]) {
+        j ++;
+        if (j - i > cnt) {
+          cnt = j - i;
+          c = str[i];
+        }
+      }
+      i = j - 1;
+    }
+
+    cout << c << ' ' << cnt << endl;
+    
+  }
+
+  return 0;
+}
