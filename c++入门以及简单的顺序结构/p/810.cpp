d429b996cc485a291ed95bb66158fee187bc2392
coolCicada <1475395755@qq.com>
2023-01-26 15:46:11 +0800

    810

---
+++ c++入门以及简单的顺序结构/p/810.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int abs(int x) {
+  if (x < 0) return -x;
+  return x;
+}
+
+int main() {
+  int n;
+  cin >> n;
+
+  cout << abs(n);
+
+  return 0;
+}
