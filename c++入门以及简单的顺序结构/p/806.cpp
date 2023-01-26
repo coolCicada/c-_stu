129545da70c356354cd5ca1f9a4c6b2769a3f988
coolCicada <1475395755@qq.com>
2023-01-26 15:48:35 +0800

    806

---
+++ c++入门以及简单的顺序结构/p/806.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+double add(double x, double y) {
+  return x + y;
+}
+
+int main() {
+  double x, y;
+  cin >> x >> y;
+
+  printf("%.2lf", add(x, y));
+
+  return 0;
+}
