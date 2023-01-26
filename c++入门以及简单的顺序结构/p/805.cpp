6b770c555b8e12a737e58531bd4d9d3b3e830f71
coolCicada <1475395755@qq.com>
2023-01-26 15:03:08 +0800

    805

---
+++ c++入门以及简单的顺序结构/p/805.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int max(int x, int y) {
+  return x > y ? x : y;
+}
+
+int main() {
+  int x, y;
+  cin >> x >> y;
+
+  cout << max(x, y) << endl;
+
+  return 0;
+}
