3cc0399cc6087a875c55dbe1e6c19fe1da042e1f
coolCicada <1475395755@qq.com>
2023-01-27 09:05:02 +0800

    完结

--- c++入门以及简单的顺序结构/99.位运算与常用库函数.cpp
+++ c++入门以及简单的顺序结构/99.位运算与常用库函数.cpp
@@ -2,7 +2,19 @@
 using namespace std;
 
 int main() {
-  
+  vector<int> a({1, 4, 2, 2, 2, 3, 4, 5});
 
+  reverse(a.begin(), a.end());
+
+  for (int i = 0; i < a.size(); i ++) {
+    cout << a[i] << ' ';
+  }
+  cout << endl;
+
+  a.erase(unique(a.begin(), a.end()), a.end());
+  for (int i = 0; i < a.size(); i ++) {
+    cout << a[i] << ' ';
+  }
+  
   return 0;
 }
