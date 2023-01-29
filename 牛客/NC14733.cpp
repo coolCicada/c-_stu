a6c45ede940b411f58296bc6e3ddc026ac0d66a2
coolCicada <1475395755@qq.com>
2023-01-29 10:05:46 +0800

    边界查找写法

--- 牛客/NC14733.cpp
+++ 牛客/NC14733.cpp
@@ -8,7 +8,7 @@ int main() {
   int n;
   cin >> n;
 
-  while (n --) {
+  /* while (n --) {
     int l, r;
     cin >> l >> r;
 
@@ -17,6 +17,36 @@ int main() {
     if (la * la == l) cnt ++;
 
     cout << cnt << endl;
+  } */
+
+  while (n --) {
+    int a, b, at, bt;
+    cin >> a >> b;
+
+    int l = 0, r = 4e4;
+
+    while (l < r) {
+      int mid = l + r + 1>> 1;
+      if (mid * mid < a) l = mid;
+      else r = mid - 1;
+    }
+
+    at = l;
+    
+    if (a == 0) at = -1;
+    
+    l = 0, r = 4e4;
+
+    while (l < r) {
+      int mid = l + r + 1 >> 1;
+      if ((double)mid * mid <= b) l = mid;
+      else r = mid - 1;
+    }
+
+    bt = l;
+    
+
+    cout << bt - at << endl;
   }
 
   return 0;
