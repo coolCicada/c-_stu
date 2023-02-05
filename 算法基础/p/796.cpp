872ecdd91ad3e7eb532b0925b671ad6472716087
coolCicada <1475395755@qq.com>
2023-02-05 11:33:51 +0800

    feat:二维前缀和模板题

---
+++ 算法基础/p/796.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  int n, m, q;
+  cin >> n >> m >> q;
+
+  vector<vector<int> > a(n + 1, vector<int>(m + 1));
+  for (int i = 1; i <= n; i ++) {
+    for (int j = 1; j <= m; j ++) {
+      cin >> a[i][j];
+    }
+  }
+
+  for (int i = 1; i <= n; i ++) {
+    for (int j = 1; j <= m; j ++) {
+      a[i][j] = a[i][j] + a[i - 1][j] + a[i][j - 1] - a[i - 1][j - 1];
+    }
+  }
+
+  while (q --) {
+    int x1, y1, x2, y2;
+    cin >> x1 >> y1 >> x2 >> y2;
+
+    cout << a[x2][y2] - a[x1 - 1][y2] - a[x2][y1 - 1] + a[x1 - 1][y1 - 1] << endl;
+  }
+  
+  return 0;
+}
